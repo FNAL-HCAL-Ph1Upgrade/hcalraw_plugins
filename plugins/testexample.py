0f2918c25d6aec7429833f68c190742b527fd966
Doyeong <doyeong@phys.ksu.edu>
2017-06-20 10:34:59 -0500

    adding mean and rms

--- plugins/testexample.py
+++ plugins/testexample.py
@@ -6,13 +6,16 @@ from shuntConvertDatasheet import *
 
 # channel selection
 INSPECT_FEDID = 1114
-CHANNEL = {"crate":34, "slot":12, "fiber":29, "fibCh":1}
+CHANNEL = {"crate":34, "slot":12, "fiber":14, "fibCh":3}
 # shunt information
-MAX_SHUNT = 31
+MAX_SHUNT = 32
 SHUNT_EVENT_UNIT = 500
 SELECTED_SHUNT = [0, 1, 2, 4, 8, 16, 18, 20, 24, 26, 28, 30, 31]
 NORM_DIC = {"NORM_FACTR" : 0.0}
 ADCtoCharge = shuntConvertDatasheet()
+# pedestal information
+PED_CHARGE_MEAN = 20.73
+PED_ADC_MEAN = 5.667 
 
 def testexample(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False, **_):
     # sanity check for incoming raw data
@@ -31,7 +34,7 @@ def testexample(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False
             header = d["header"]
             evt = header["EvN"]
             shunt = (evt-1)//SHUNT_EVENT_UNIT
-
+            print "EVENT", evt
             # sanity checks for these chunks
             for block in blocks:
                 if type(block) is not dict:
@@ -40,25 +43,15 @@ def testexample(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False
                 elif "channelData" not in block:
                     printer.warning("FED %d block has no channelData" % fedId)
                     continue
-                
+                # pick a channel >> L48
+                if block["Crate"] != CHANNEL["crate"] or block["Slot"] != CHANNEL["slot"]:
+                    continue
+            
                 for channelData in block["channelData"].values():  # channel loop
-                    # print "evt", evt, block["Slot"], channelData["Fiber"], channelData["FibCh"]
-                    # === from here ===
-                    if block["Crate"] != 34:
+                    # pick a channel >> L43 
+                    if channelData["Fiber"] != CHANNEL["fiber"] or channelData["FibCh"] != CHANNEL["fibCh"]:
                         continue
-                    """
-                    fib = 0
-                    if block["Slot"] == 12:
-                        fib += 12 + channelData["Fiber"] - 1
-                        if 13 <= channelData["Fiber"]:  
-                            fib -= 2
-                    elif block["Slot"] == 11 and 12 <= channelData["Fiber"]:  
-                        fib += channelData["Fiber"] - 12 
-                    else:  
-                        continue """
-                    # === to here === > HEP 17 / this result in slot 11 has 0~11 fib and slot 12 has 12~31 fib
-
-
+                    # print block["Crate"], block["Slot"], channelData["Fiber"], channelData["FibCh"]
                     if channelData["QIE"]:
                         # check the error flags
                         errf = "ErrFNZ" if channelData["ErrF"] else "ErrF0"
@@ -74,41 +67,66 @@ def testexample(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False
                             book.fill((ts, adc), "ADC_vs_TS_%s_%d_%d_%d_%d_%d" % (errf, fedId, block["Crate"], block["Slot"], channelData["Fiber"], channelData["FibCh"]),
                                       (nTsMax, nAdcMax), (-0.5, -0.5), (nTsMax - 0.5, nAdcMax - 0.5),
                                       title="FED %d (ErrF %s 0);time slice;ADC;Counts / bin" % (fedId, eq))
-                            # fill a 2d histogram where bins on y-axis are ADCs and bins on x-axis are shunt setting 
-                            book.fill((shunt, adc), "ADC_vs_Shunt_%s_%d_%d_%d_%d_%d" % (errf, fedId, block["Crate"], block["Slot"], channelData["Fiber"], channelData["FibCh"]),
-                                      (nTsMax, nAdcMax), (-0.5, -0.5), (nTsMax - 0.5, nAdcMax - 0.5),
-                                      title="FED %d (ErrF %s 0);shunt setting;ADC;Counts / bin" % (fedId, eq))
+                            if ts is 3: # use max time slice
+                                # fill a 2d histogram where bins on y-axis are ADCs and bins on x-axis are shunt setting 
+                                book.fill((shunt, adc), "ADC_vs_Shunt_%s_%d_%d_%d_%d_%d" % (errf, fedId, block["Crate"], block["Slot"], channelData["Fiber"], channelData["FibCh"]),
+                                          (MAX_SHUNT, nAdcMax), (0, -0.5), (MAX_SHUNT, nAdcMax - 0.5),
+                                          title="FED %d (ErrF %s 0);shunt setting;ADC;Counts / bin" % (fedId, eq))
 
 
                         # pedestal subtraction : for each event, the pedestal was estimated by the average of the first two time samples.
                         charge = 0.0
+                        ADC_pedsub = 0.0
                         pedCharge = 0.0
+                        pedADC = 0.0
+                        rms_pedADC = 0.0
                         rms_pedCharge = 0.0
-                        time_slice = {"ped_ts":[0, 1], "sum_ts":[3,4,5,6]}    # The total charge collected for each channel per event, q, was q = (TS3+TS4+TS5+TS6)-{(TS0+TS1)/2}
+                        time_slice = {"ped_ts":[0, 1], "sum_ts":[3]}#,4,5,6]}    # The total charge collected for each channel per event, q, was q = (TS3+TS4+TS5+TS6)-{(TS0+TS1)/2}
                         ped_tsNum = len(time_slice["ped_ts"])
                         sum_tsNum = len(time_slice["sum_ts"])
                         if shunt in SELECTED_SHUNT:
                             for (ts, adc) in enumerate(channelData["QIE"]):
                                 if ts in time_slice["ped_ts"]:
+                                    pedADC += adc
                                     pedCharge += float(ADCtoCharge[shunt][adc])
                                     if ts is time_slice["ped_ts"][-1]:
+                                        pedADC /= ped_tsNum
                                         pedCharge /= ped_tsNum
+                                        rms_pedADC = ((pedADC-PED_ADC_MEAN)**2)**0.5
+                                        rms_pedCharge = ((pedCharge-PED_CHARGE_MEAN)**2)**0.5
                                 if ts in time_slice["sum_ts"]:
+                                    ADC_pedsub += adc
                                     charge += float(ADCtoCharge[shunt][adc])
                                     if ts is time_slice["sum_ts"][-1]:
+                                        ADC_pedsub -= pedADC
                                         charge -= pedCharge
 
                             # fill a 2d histogram where bins on y-axis are charges and bins on x-axis are GSel ; pedestal was subtracted 
+                            book.fill((shunt, ADC_pedsub), "ADC_vs_GSel_%s_%d_%d_%d_%d_%d" % (errf, fedId, block["Crate"], block["Slot"], channelData["Fiber"], channelData["FibCh"]),
+                                      (MAX_SHUNT, nAdcMax), (0, -0.5), (MAX_SHUNT, nAdcMax-0.5),
+                                      title="FED %d (ErrF %s 0);GSel;ADC;Counts / bin" % (fedId, eq))                                
                             book.fill((shunt, charge), "Charge_vs_GSel_%s_%d_%d_%d_%d_%d" % (errf, fedId, block["Crate"], block["Slot"], channelData["Fiber"], channelData["FibCh"]),
-                                      (nTsMax, 200), (-0.5, 0), (nTsMax - 0.5, 130000),
+                                      (MAX_SHUNT, 200), (0, 70000), (MAX_SHUNT, 100000),
                                       title="FED %d (ErrF %s 0);GSel;Charge[fC];Counts / bin" % (fedId, eq))                                
-                            
-                            if shunt is 0:
+
+                            if shunt == 0:
                                 # fill mean fedestal
-                                book.fill(pedCharge, "pedCharge_%s_%d" % (errf, fedId),
-                                          200, 0, 130000,
+                                book.fill(pedADC, "ave_pedADC_%s_%d" % (errf, fedId),
+                                          9, 3, 12,
+                                          title="FED %d (ErrF %s 0);pedCharge;Counts / bin" % (fedId, eq))                                
+                                book.fill(pedCharge, "ave_pedCharge_%s_%d" % (errf, fedId),
+                                          200, 10, 30,
+                                          title="FED %d (ErrF %s 0);pedCharge;Counts / bin" % (fedId, eq))                                
+                                book.fill(rms_pedADC, "ave_RMS_pedADC_%s_%d" % (errf, fedId),
+                                          20, 0, 1,
+                                          title="FED %d (ErrF %s 0);pedCharge;Counts / bin" % (fedId, eq))                                
+                                print rms_pedCharge
+                                book.fill(rms_pedCharge, "ave_RMS_pedCharge_%s_%d" % (errf, fedId),
+                                          2500, 0, 25,
                                           title="FED %d (ErrF %s 0);pedCharge;Counts / bin" % (fedId, eq))                                
 
 
 
 
+
+                            
