4fa4ca5005d1a534b31268e2c258f0a0a10e288b
Doyeong <doyeong@phys.ksu.edu>
2017-06-22 11:14:19 -0500

    added TProfile option

--- plugins/histo_dy.py
+++ plugins/histo_dy.py
@@ -5,16 +5,21 @@ import math
 from pprint import pprint
 from shuntConvertDatasheet import *
 
+""" =========================================================================
+
+
+========================================================================= """
+
+
 # channel selection
 INSPECT_FEDID = 1114
-CHANNEL = {"crate":34, "slot":12, "fiber":14, "fibCh":3}
+CHANNEL = {"crate":34, "slot":12, "fiber":22, "fibCh":0}
 # shunt information
 MAX_SHUNT = 32
-SHUNT_EVENT_UNIT = 500
+SHUNT_EVENT_UNIT = 500 # evt1~500 = shunt 0, evt501~1000 = shunt1, etc..
 SELECTED_SHUNT = [0, 1, 2, 4, 8, 16, 18, 20, 24, 26, 28, 30, 31]
-NORM_DIC = {"NORM_FACTR" : 0.0}
 ADCtoCharge = shuntConvertDatasheet()
-# pedestal information
+# pedestal information 
 PED = { }
 
 def histo_dy(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False, **_):
@@ -34,7 +39,8 @@ def histo_dy(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False, *
             header = d["header"]
             evt = header["EvN"]
             shunt = (evt-1)//SHUNT_EVENT_UNIT
-            print "EVENT", evt
+            if evt%SHUNT_EVENT_UNIT == 0: 
+                print "EVENT", evt
             # sanity checks for these chunks
             for block in blocks:
                 if type(block) is not dict:
@@ -49,7 +55,7 @@ def histo_dy(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False, *
             
                 for channelData in block["channelData"].values():  # channel loop
                     # pick a channel >> L43 
-                    # if channelData["Fiber"] != CHANNEL["fiber"] or channelData["FibCh"] != CHANNEL["fibCh"]:
+                    #if channelData["Fiber"] != CHANNEL["fiber"] or channelData["FibCh"] != CHANNEL["fibCh"]:
                     #    continue
                     
                     # channel information in string form
@@ -58,27 +64,38 @@ def histo_dy(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False, *
                     if channelData["QIE"]:
                         # check the error flags
                         errf = "ErrFNZ" if channelData["ErrF"] else "ErrF0"
-
                         # some logic for naming two histograms, one for a clean error flag and another with a problematic error flag
                         eq = "!=" if channelData["ErrF"] else "=="
-                        
+
+                        # delet this if you want to make plots of channel with error
+                        if errf == "ErrFNZ":
+                            continue
+                         
                         nAdcMax = 256
+                        ts_max = 3 # you can find this time slice by seeing pulse shape plot whici is ADC vs TS (or charge vs TS)
                         for (ts, adc) in enumerate(channelData["QIE"]): # TS loop : each channel is consist of 10TS
                             if nTsMax <= ts:
                                 break
                             # fill a 2d histogram where bins on y-axis are ADCs and bins on x-axis are time slice index 
                             book.fill((ts, adc), "ADC_vs_TS_%s_%d_%s" % (errf, fedId, str_ch),
-                                      (nTsMax, nAdcMax), (-0.5, -0.5), (nTsMax - 0.5, nAdcMax - 0.5),
-                                      title="FED %d (ErrF %s 0);time slice;ADC;Counts / bin" % (fedId, eq))
-                            if ts is 3: # use max time slice
+                                      #(nTsMax, nAdcMax), (-0.5, -0.5), (nTsMax - 0.5, nAdcMax - 0.5),    # active this to make 2D histogram
+                                      nTsMax, -0.5, nTsMax - 0.5,                                         # active this to make TProfile
+                                      title="FED %d_%s (ErrF %s 0);time slice;ADC;Counts / bin" % (fedId, str_ch , eq))
+                            # fill a TProfile where bins on y-axis are Charge
+                            if shunt in SELECTED_SHUNT:
+                                charge = float(ADCtoCharge[shunt][adc])
+                                book.fill((ts, charge), "Charge_vs_TS_%s_%d_%s" % (errf, fedId, str_ch),
+                                          nTsMax, -0.5, nTsMax-0.5,
+                                          title="FED %d_%s (ErrF %s 0);time slice;Charge[fC];Counts / bin" % (fedId, str_ch , eq))
+                            if ts is ts_max: # use max time slice
                                 # fill a 2d histogram where bins on y-axis are ADCs and bins on x-axis are shunt setting 
                                 book.fill((shunt, adc), "ADC_vs_Shunt_%s_%d_%s" % (errf, fedId, str_ch),
                                           (MAX_SHUNT, nAdcMax), (0, -0.5), (MAX_SHUNT, nAdcMax - 0.5),
-                                          title="FED %d (ErrF %s 0);shunt setting;ADC;Counts / bin" % (fedId, eq))
+                                          title="FED %d_%s (ErrF %s 0);shunt setting;ADC;Counts / bin" % (fedId, str_ch, eq))
 
 
                         # pedestal subtraction : for each event, the pedestal was estimated by the average of the first two time samples.
-                        charge = 0.0
+                        charge_pedsub = 0.0
                         ADC_pedsub = 0.0
                         pedCharge = 0.0
                         pedADC = 0.0
@@ -95,36 +112,40 @@ def histo_dy(raw1={}, raw2={}, book=None, warnQuality=True, fewerHistos=False, *
                                         pedCharge /= ped_tsNum
                                 if ts in time_slice["sum_ts"]:
                                     ADC_pedsub += adc
-                                    charge += float(ADCtoCharge[shunt][adc])
+                                    charge_pedsub += float(ADCtoCharge[shunt][adc])
                                     if ts is time_slice["sum_ts"][-1]:
                                         ADC_pedsub -= pedADC
-                                        charge -= pedCharge
+                                        charge_pedsub -= pedCharge
                             # accumulate mean value of pedestal for each channel, ( ADC(or charge)sum of corresponding TS / #TS ) 
                             if not(str_ch in PED):
                                 PED[str_ch] = []
                             PED[str_ch].append(pedADC) 
 
-                            # fill a 2d histogram where bins on y-axis are charges and bins on x-axis are GSel ; pedestal was subtracted 
+                            # fill 2D histograms where bins on y-axis are charges and bins on x-axis are GSel ; pedestal was subtracted 
                             book.fill((shunt, ADC_pedsub), "ADC_vs_GSel_%s_%d_%s" % (errf, fedId, str_ch),
-                                      (MAX_SHUNT, nAdcMax), (0, -0.5), (MAX_SHUNT, nAdcMax-0.5),
-                                      title="FED %d (ErrF %s 0);GSel;ADC;Counts / bin" % (fedId, eq))                                
-                            book.fill((shunt, charge), "Charge_vs_GSel_%s_%d_%s" % (errf, fedId, str_ch),
-                                      (MAX_SHUNT, 200), (0, 20000), (MAX_SHUNT, 100000),
-                                      title="FED %d (ErrF %s 0);GSel;Charge[fC];Counts / bin" % (fedId, eq))
-        # pedestal mean and RMS 
-        if evt == SHUNT_EVENT_UNIT:
-            for ch, adclist in PED.items():
-                ped_mean = sum(adclist)/len(adclist)
-                vsum = 0
-                for x in adclist:
-                    vsum = vsum + (x-ped_mean)**2
-                var = vsum/len(adclist)
-                ped_rms = math.sqrt(var)
-
-                book.fill(ped_mean, "mean_pedADC_%s_%d" % (errf, fedId),
-                          50, 0, 10,
-                          title="FED %d (ErrF %s 0);pedestal mean;Counts / bin" % (fedId, eq)) 
-                book.fill(ped_rms, "RMS_pedADC_%s_%d" % (errf, fedId),
+                                      # (MAX_SHUNT, nAdcMax), (0, -0.5), (MAX_SHUNT, nAdcMax-0.5),                    # active this to make 2D histogram 
+                                      MAX_SHUNT, 0, MAX_SHUNT,                                                       # active this to make TProfile 
+                                      title="FED %d_%s (ErrF %s 0);GSel;ADC;Counts / bin" % (fedId, str_ch, eq))                                
+                            book.fill((shunt, charge_pedsub), "Charge_vs_GSel_%s_%d_%s" % (errf, fedId, str_ch),
+                                      # (MAX_SHUNT, 200), (0, 20000), (MAX_SHUNT, 100000),                            # active this to make 2D histogram 
+                                      MAX_SHUNT, 0, MAX_SHUNT,                                                       # active this to make TProfile 
+                                      title="FED %d_%s (ErrF %s 0);GSel;Charge[fC];Counts / bin" % (fedId, str_ch, eq))
+            print "# of channel", len(PED)
+        
+            # pedestal mean and RMS 
+            if evt == SHUNT_EVENT_UNIT:
+                for ch, adclist in PED.items():
+                    ped_mean = sum(adclist)/len(adclist)
+                    vsum = 0
+                    for x in adclist:
+                        vsum = vsum + (x-ped_mean)**2
+                    var = vsum/len(adclist)
+                    ped_rms = math.sqrt(var)
+                    # fill 1D histograms 
+                    book.fill(ped_mean, "mean_pedADC_%s_%d" % (errf, fedId),
+                              50, 0, 10,
+                              title="FED %d (ErrF %s 0);pedestal mean;Counts / bin" % (fedId, eq)) 
+                    book.fill(ped_rms, "RMS_pedADC_%s_%d" % (errf, fedId),
                               50, 0, 5,
                               title="FED %d (ErrF %s 0);pedestal RMS;Counts / bin" % (fedId, eq))                             
 
